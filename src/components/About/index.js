d213dbaa5473a7e680c97ad6fa59800124d43d22
Enrique Jimenez <ejimenez2292@gmail.com>
2022-06-25 22:58:41 -0500

    styled about me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,10 +5,13 @@ function About() {
     return (
         <>
         <SideNav />
-        <section id="About">
+        <section className="aboutMe" id="About">
             <h1>About Me</h1>
             <p>Software Engineer adept in all stages of advanced web development. Knowledgeable in the user interface, testing, and debugging processes. Equipped with a diverse and promising skill-set. Proficient in an assortment of technologies, including JavaScript,  REACT,  MERN, and MySQL. Able to effectively self-manage during independent projects and collaborate in a team setting.</p>
         </section>
+        <div className="avatar">
+            <img alt="avatar" className="avatar" src={require('../../assets/images/bot.jpg')}/>
+        </div>
         </>
     )
 }
