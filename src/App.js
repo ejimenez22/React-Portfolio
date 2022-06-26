d76cee0c41e8c0c40b7250bfe7b934efd095c6e0
Enrique Jimenez <ejimenez2292@gmail.com>
2022-06-25 22:22:20 -0500

    refactored index and app

--- src/App.js
+++ src/App.js
@@ -5,21 +5,23 @@ import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
 import Resume from './components/Resume';
 import Home from './components/Home';
-import { Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
 
-  return (
-      <>
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/about' element={<About />} />
-      <Route path='/portfolio' element={<Portfolio />} />
-      <Route path='/resume' element={<Resume />} />
-      <Route path='/contact' element={<Contact />} />
-    </Routes>
-    </>
-  )
+  return (
+      <>
+      <BrowserRouter basename='/'>
+        <Routes>
+          <Route path='/*' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/portfolio' element={<Portfolio />} />
+          <Route path='/resume' element={<Resume />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+    </BrowserRouter>
+    </>
+  )
 }
 
 export default App;
