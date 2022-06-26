e01d0221559d2d1849967f926041e1d0402d16d5
Enrique Jimenez <ejimenez2292@gmail.com>
2022-06-26 00:22:03 -0500

    styled code, need to add email val

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -5,23 +5,41 @@ function Contact() {
     return (
         <>
         <SideNav />
-        <section id='Contact'>
-            <h1>Contact</h1>
-            <form id='contact-form'className='contactFormEntry' >
+        <section className='contactForm' id='Contact'>
+            <h1>Contact Me</h1>
+            <form id='contact-form' className='form'>
                 <div>
                     <label htmlFor='name'>Name:</label>
-                    <input type='text' name='name'></input>
+                    <br />
+                    <input type='text' name='name' className='nameInput'></input>
                 </div>
                 <div>
                     <label htmlFor='email'>Email Address:</label>
-                    <input type='email' name='email'></input>
+                    <br />
+                    <input type='email' name='email' className='email'></input>
                 </div>
                 <div>
                     <label htmlFor='message'>Message:</label>
-                    <input rows='10' message='message'></input>
+                    <br />
+                    <textarea name='message' rows='5' />
                 </div>
+                <button type='submit'>Submit</button>
             </form>
         </section>
+        <div >
+            <ul className='myLinks'>
+                <li>
+                <a href="https://github.com/ejimenez22" target="_blank" rel="noopener noreferrer">
+                <img className="contactImg" alt="Git" src={require("../../assets/images/Git.png")} />
+                </a>
+                </li>
+                <li>
+                <a href="https://www.linkedin.com/in/enrique-jimenez-0121a5177/" target="_blank" rel="noopener noreferrer">
+                <img className="contactImg" id='linkedinImg' alt="Linkedin" src={require("../../assets/images/Linkedin.png")} />
+                </a>
+                </li>
+            </ul>
+        </div>
         </>
     )
 }
